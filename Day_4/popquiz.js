9c67ad833f5a4696a8d8598550b7999073647725
william-coleman <williamcoleman@WilliamColemanMBP.local>
2016-09-20 11:15:23 -0400

    Week1 commit

---
+++ Day_4/popquiz.js
@@ -0,0 +1,20 @@
+var memory = Math.floor(Math.random() * (50 - 25) + 25);
+            ((memory - 25) * 100) / (50 - 25);
+var difficulty = 35;
+
+console.log(memory);
+
+if (memory >= difficulty) {
+    console.log("Congradulations!");
+} else {
+    console.log("Study up and try again.");
+}
+
+// Math.random() * (50 - 25) + 25;
+// ((score - 25) * 100) / (50 - 25);
+// var studentScore = 47;
+// if (studentScore > 50) {
+//     console.log("Congradulations");
+// } else {
+//     console.log("Study up next time.");
+// }
